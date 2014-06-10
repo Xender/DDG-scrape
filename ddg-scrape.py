117aecc93515e0022d472b977b88b116814078e9
Aleksander Nitecki <ixendr@itogi.re>
2014-06-10 12:10:56 +0200

    Don't hardcode query, use argv.

--- ddg-scrape.py
+++ ddg-scrape.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
+import sys
+
 import requests
 from bs4 import BeautifulSoup
 
 ddg_url = "https://duckduckgo.com/html/"
 
-keyword = 'cars'
-
-def main():
-    r = requests.get(ddg_url, params={'q': keyword})
+def search(query):
+    r = requests.get(ddg_url, params={'q': query})
     r.raise_for_status()
 
     soup = BeautifulSoup(r.text)
     for result in soup.findAll('div', class_='results_links'):
-        print(result.find(class_='large').text)
+        link = result.find('a', class_='large')
+        print(link.text)
+        print(link['href'])
         print(result.find(class_='snippet').text)
         print()
 
 if __name__ == "__main__":
-    main()
+    search(sys.argv[1])
